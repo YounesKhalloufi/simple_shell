1dc6fb5b07646825fa877edd365fc9a5acebabd4
Younes_Khalloufi <younes.khaloufi.1998@gmail.com>
2023-07-14 09:22:17 -0700

    added more functions

---
+++ _functions1.c
@@ -0,0 +1,138 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ * @n: the amount of chars to copy
+ *
+ * Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j;
+	char *s = dest;
+
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: the destination
+ * @src: the source
+ * @n: the amount of bytes to be maximally used
+ *
+ * Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+	{
+		dest[i] = '\0';
+	}
+	return (s);
+}
+
+/**
+ * _strchr - Locates a char in a string
+ * @s: the string
+ * @c: the char to look for
+ *
+ * Return: a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+		{
+			return (s);
+		}
+	} while (*s++ != '\0');
+	return (NULL);
+}
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the pointer to the memory area
+ * @b: the byte to fill *s with
+ * @n: the amount of bytes to be filled
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: a pointer.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+	{
+		return (malloc(new_size));
+	}
+	if (!new_size)
+	{
+		return (free(ptr), NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	p = malloc(new_size);
+	if (!p)
+	{
+		return (NULL);
+	}
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+	{
+		p[old_size] = ((char *)ptr)[old_size];
+	}
+	free(ptr);
+	return (p);
+}
